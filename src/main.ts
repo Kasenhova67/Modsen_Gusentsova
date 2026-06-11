4800b5f3fa5263a806b7b6f6846618535ffd611f
Kasenhova67 <katya.gusentsova84@gmail.com>
2026-06-11 17:52:36 +0300

    Docker+Swager

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(3000);
   console.log('Сервер запущен на http://localhost:3000');
 }
