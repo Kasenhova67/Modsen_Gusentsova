12b46f212a3cbb0dcc459156593af42cfd1cd077
Ekaterina Gusentsova <159613400+Kasenhova67@users.noreply.github.com>
2026-06-18 17:41:01 +0300

    fix: use DATABASE_URL for Railway deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CategoriesModule } from './categories/categories.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { SummaryModule } from './summary/summary.module';
 import { Category } from './categories/category.entity';
 import { Transaction } from './transactions/transaction.entity';
-import { SummaryModule } from './summary/summary.module';
 
 @Module({
   imports: [
@@ -14,12 +14,12 @@ import { SummaryModule } from './summary/summary.module';
       imports: [ConfigModule],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: config.get('DB_HOST'),
-        port: config.get('DB_PORT'),
-        username: config.get('DB_USER'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_NAME'),
+        url: config.get('DATABASE_URL'), 
         entities: [Category, Transaction],
+        synchronize: true,
+        ssl: {
+          rejectUnauthorized: false, 
+        },
       }),
       inject: [ConfigService],
     }),
